175fd1d4c93b621d39d6f0a1e4009742ca89618c
Juntao Zhang <juntao.zhang.cn@gmail.com>
2017-07-10 14:00:34 +0800

    my lintcode

---
+++ src/main/java/Maximum_Subarray_III.java
@@ -0,0 +1,60 @@
+/*
+
+Given an array of integers and a number k, find k non-overlapping subarrays which have the largest sum.
+
+The number in each subarray should be contiguous.
+
+Return the largest sum.
+
+Have you met this question in a real interview? Yes
+Example
+Given [-1,4,-2,3,-2,3], k=2, return 8
+
+Note
+The subarray should contain at least one number
+
+Tags Expand 
+LintCode Copyright Dynamic Programming Subarray Array
+*/
+
+import java.util.ArrayList;
+
+/*
+	NOT DONE
+*/
+public class Maximum_Subarray_III {
+    /**
+     * @param nums: A list of integers
+     * @param k: An integer denote to find k non-overlapping subarrays
+     * @return: An integer denote the sum of max k non-overlapping subarrays
+     */
+    public int maxSubArray(int[] nums, int k) {
+        if (nums.length < k) {
+            return 0;
+        }
+        int len = nums.length;
+
+
+        int[][] globalMax = new int[k + 1][len + 1];
+        int[][] localMax = new int[k + 1][len + 1];
+
+        for (int i = 1; i <= k; i++) {
+            localMax[i][i-1] = Integer.MIN_VALUE;
+            //小于 i 的数组不能够partition
+            for (int j = i; j <= len; j++) {
+                localMax[i][j] = Math.max(localMax[i][j-1], globalMax[i - 1][j-1]) + nums[j-1];
+                if (j == i)
+                    globalMax[i][j] = localMax[i][j];
+                else
+                    globalMax[i][j] = Math.max(globalMax[i][j-1], localMax[i][j]);
+            }
+        }
+        return globalMax[k][len];
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new Maximum_Subarray_III().maxSubArray(new int[]{-1,4,-2,3,-2,3},2));
+    }
+}
+
---
+++ src/test/java/cn/juntaozhang/lintcode/Read_Characters_From_File.java
@@ -0,0 +1,10 @@
+package cn.juntaozhang.lintcode;
+
+/**
+ * @author juntao zhang
+ */
+public class Read_Characters_From_File {
+    public static void main(String[] args) {
+
+    }
+}
