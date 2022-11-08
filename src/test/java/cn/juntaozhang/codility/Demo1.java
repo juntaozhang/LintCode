1edef9acd44ca9a4295ff69d617467aca2bb6848
juntao <juntzhang@foxmail.com>
2022-11-08 22:41:37 +0800

    greedy & sort

--- src/test/java/cn/juntaozhang/codility/Demo1.java
+++ src/test/java/cn/juntaozhang/codility/Demo1.java
@@ -9,8 +9,8 @@ import java.util.Arrays;
  */
 public class Demo1 {
     @Test
-    public void test(){
-        System.out.println(backPack(10,new int[]{3}));
+    public void test() {
+        System.out.println(backPack(10, new int[]{3}));
     }
 
     public int backPack(int m, int[] A) {
@@ -28,5 +28,25 @@ public class Demo1 {
             }
         }
         return dp[m];
+    }
+
+    @Test
+    public void longestCommonSubsequence() {
+        System.out.println(longestCommonSubsequence("ABCD", "EACB"));
+    }
+
+    public int longestCommonSubsequence(String A, String B) {
+        // dp[i][j] = dp[i] == dp[j] ? dp[i - 1][j - 1] + 1: max( dp[i - 1][j],  dp[i][j - 1])
+        int[][] dp = new int[A.length() + 1][B.length() + 1];
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (A.charAt(i - 1) == B.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[A.length()][B.length()];
     }
 }
